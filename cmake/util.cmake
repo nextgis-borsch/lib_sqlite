745a91ba971e468dba18e9e12af5f4a01482aa86
Dmitry Baryshnikov <bishop.dev@gmail.com>
2019-08-10 20:40:06 +0300

    Update borsch script

--- cmake/util.cmake
+++ cmake/util.cmake
@@ -105,9 +105,14 @@ endfunction()
 
 function(get_cpack_filename ver name)
     get_compiler_version(COMPILER)
-    get_prefix(STATIC_PREFIX BUILD_STATIC_LIBS)
+    
+    if(NOT DEFINED BUILD_STATIC_LIBS)
+      set(BUILD_STATIC_LIBS OFF)
+    endif()
 
-    set(${name} ${PROJECT_NAME}-${ver}-${STATIC_PREFIX}${COMPILER} PARENT_SCOPE)
+    get_prefix(STATIC_PREFIX ${BUILD_STATIC_LIBS})
+
+    set(${name} ${PACKAGE_NAME}-${ver}-${STATIC_PREFIX}${COMPILER} PARENT_SCOPE)
 endfunction()
 
 function(get_compiler_version ver)
